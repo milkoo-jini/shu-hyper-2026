a8afee6c9d6bf32889780d55732630879ce9c582
milkoo-jini <go9oomi@naver.com>
2026-04-13 21:42:10 +0900

    Update page_keyword.py

--- page_keyword.py
+++ page_keyword.py
@@ -7,7 +7,7 @@ from google.genai import types
 from collections import deque
 
 def run_keyword():
-    st.markdown("<style>div.block-container{padding-top:1rem;}</style>", unsafe_allow_html=True)
+    st.markdown("<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True)
 
     with st.sidebar:
         st.markdown("### 🔐 관리자 인증")
