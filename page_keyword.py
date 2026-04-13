2133ba2b886dd5868fb3aeb4972e3b7343a24829
milkoo-jini <go9oomi@naver.com>
2026-04-13 22:49:48 +0900

    Update page_keyword.py

--- page_keyword.py
+++ page_keyword.py
@@ -207,7 +207,7 @@ def run_keyword():
 {seed}여론조작의혹"""
 
                     response = client.models.generate_content(
-                        model="gemini-1.5-flash",
+                        model="gemini-2.5-flash-lite-preview-06-17",
                         contents=prompt,
                         config=types.GenerateContentConfig(
                             tools=[types.Tool(google_search=types.GoogleSearch())]
